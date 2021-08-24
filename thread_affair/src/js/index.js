6a1e8ca29ad0837f438aad58fa5c73337d8da4be
patris-patras <patras.patricia@gmail.com>
2021-08-24 22:11:50 +0300

    live thread-affair with patches

--- thread_affair/src/js/index.js
+++ thread_affair/src/js/index.js
@@ -1,1 +1,10 @@
+import Glide from '@glidejs/glide';
 import './../scss/index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const glideInstance = new Glide('.glide', {
+    type: 'carousel',
+  });
+
+  glideInstance.mount({ Controls: Controls });
+});
